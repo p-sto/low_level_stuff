ffcfff840b5528ee2ba7079749d26021748d96bc
stovorov <stoworow@gmail.com>
2020-01-21 14:09:08 +0100

    working on lib

--- hello_world.asm
+++ hello_world.asm
@@ -7,27 +7,11 @@ section .data:
 	text: db 'Hello, World', 10, 0
 
 _start:
-	mov rax, text
-	call	_print	
+	GETSTRLEN text
+	mov	rdx, rax
+	PRINT text, rdx
+	
 
 	EXIT 0
-
-_print:
-	push 	rax
-	mov 	rbx, 0	
-_printloop:
-	inc	rax
-	inc	rbx		; rbx is used to count lenght of string
-	mov	cl, [rax]	; cl is 8-bit part of rcx
-	cmp	cl, 0
-	jne	_printloop
-
-	mov	rax, SYS_WRITE
-	mov	rdi, STDOUT
-	pop	rsi
-	mov	rdx, rbx
-	syscall
-
-	ret
 
 
